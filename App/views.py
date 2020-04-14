844ab2548bd449f4502919046674422d63bb91e2
shijf <shijf@broada.com>
2020-04-14 18:50:27 +0800

    test

--- App/views.py
+++ App/views.py
@@ -10,7 +10,23 @@ from django.views import View
 
 from constant import (URGENT_LEVEL, MODEL_ID, DESCRIPTION, ROUTE_ID, LOOP_ID,
                       USER_ID, WEEBHOOK, PRIVATE_TOKEN, API_KEY_ONE,
-                      API_KEY_TWO, WORK_ORDER_URL, TENANT_URL)
+                      API_KEY_TWO, WORK_ORDER_URL, TENANT_URL, BASE_URL)
+
+
+def send_merge_message(data, target_url):
+    get_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
+    sendInfoTitle = "发送工单提交信息!"
+    sendInfoText = "## **{}发起了mr通知** \n" \
+                   "+ 审批人:&ensp;{}&ensp;{}\n" \
+                   "+ [点击查看]({})&ensp;{}".format(
+        data['user']['username'], check_user_one, check_user_two,
+        target_url, get_time)
+
+    at_mobiles = [mobile_one, mobile_two]
+    xiaoding = DingtalkChatbot(WEEBHOOK)
+    xiaoding.send_markdown(title=sendInfoTitle,
+                           text=sendInfoText,
+                           at_mobiles=at_mobiles)
 
 
 class Tenate(object):
@@ -26,13 +42,14 @@ tenant = Tenate()
 result_one = tenant.get_tenant_message(API_KEY_ONE)
 result_two = tenant.get_tenant_message(API_KEY_TWO)
 check_user_one = result_one[0]
-mobile_one = result_one[1]
+# mobile_one = result_one[1]
+mobile_one = "18365597692"
 check_user_two = result_two[0]
-mobile_two = result_two[0]
+# mobile_two = result_two[0]
+mobile_two = "18365597692"
 
 
 class Gitlab(object):
-
     def get_merge(self, data):
         if data["object_attributes"]["action"] == "open" or \
                         data["object_attributes"]["action"] == "reopen":
@@ -56,9 +73,12 @@ class Gitlab(object):
                     "webhook": WEEBHOOK,
                     "username": data['user']['username'],
                     "checkuserone": result_one[0],
-                    "mobileone": result_one[1],
+                    # "mobileone": result_one[1],
                     "checkusertwo": result_two[0],
-                    "mobiletwo": result_two[1],
+                    # "mobiletwo": result_two[1],
+                    "mobileone": "18365597692",
+                    "mobiletwo": "18365597692",
+
                 },
                 "handle_rules": {
                     "route_id": ROUTE_ID,
@@ -71,42 +91,12 @@ class Gitlab(object):
                 }
             }
             try:
-                requests.post(url=WORK_ORDER_URL, json=body)
+                response = requests.post(url=WORK_ORDER_URL, json=body)
+                response_json = response.json()
+                target_url = "{}/{}".format(BASE_URL, response_json["id"])
+                send_merge_message(data, target_url)
             except Exception as e:
                 print(e)
-
-
-class Ding(object):
-
-    def send_merge_message(self, data, target_url):
-        get_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
-        sendInfoTitle = "发送工单提交信息!"
-        sendInfoText = "## **{}发起了mr通知** \n" \
-                       "+ 审批人:&emsp;{}&emsp;{}\n" \
-                       "+ [点击查看]({})&emsp;{}".format(
-            data['user']['username'], check_user_one, check_user_two,
-            target_url, get_time)
-
-        at_mobiles = [mobile_one, mobile_two]
-        xiaoding = DingtalkChatbot(WEEBHOOK)
-        xiaoding.send_markdown(title=sendInfoTitle,
-                               text=sendInfoText,
-                               at_mobiles=at_mobiles)
-
-    def send_merge_result_message(self, username, result, merge_url):
-        get_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time()))
-        sendInfoTitle = "发送工单合并结果信息!"
-        sendInfoText = "## **{}发起了mr通知** \n" \
-                       "+ 审批人:&emsp;{}&emsp;{}\n" \
-                       "+ 执行结果:&ensp;{}\n" \
-                       "+ [点击查看]({})&emsp;{}".format(username, check_user_one,
-                                                     check_user_two, result,
-                                                     merge_url, get_time)
-        at_mobiles = [mobile_one, mobile_two]
-        xiaoding = DingtalkChatbot(WEEBHOOK)
-        xiaoding.send_markdown(title=sendInfoTitle,
-                               text=sendInfoText,
-                               at_mobiles=at_mobiles)
 
 
 class IndexView(View):
@@ -118,12 +108,12 @@ class IndexView(View):
         try:
             content = request.body
             data = json.loads(content)
-            print("data==================", data)
             gitlab = Gitlab()
             gitlab.get_merge(data)
         except Exception as e:
             data = ''
             print(e)
         return HttpResponse('index')
+
 
 
